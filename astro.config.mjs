ae72e72b5c8fdf03317ee1fcd64140ee66473902
Aaron <aaronye1001@gmail.com>
2025-03-29 18:11:04 -0400

    Add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@
 import { defineConfig } from 'astro/config';
 import { visualizer } from "rollup-plugin-visualizer";
 
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
     vite: {
         plugins: [visualizer({
             emitFile: true,
             filename: "stats.html",
-        })]
+        }), tailwindcss()]
     }
 });
