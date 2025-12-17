2070785ddad7390ec9ab5adca97cae814fa5ffe9
Aaron <aaronye1001@gmail.com>
2025-12-16 23:13:25 -0500

    Change default styling and add svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,15 @@ import { visualizer } from "rollup-plugin-visualizer";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import svelte from "@astrojs/svelte";
+
 export default defineConfig({
-    vite: {
-        plugins: [visualizer({
-            emitFile: true,
-            filename: "stats.html",
-        }), tailwindcss()]
-    }
+  vite: {
+      plugins: [visualizer({
+          emitFile: true,
+          filename: "stats.html",
+      }), tailwindcss()]
+  },
+
+  integrations: [svelte()]
 });
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,11 @@
 export default {
     content: [],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                sans: ["Instrument Sans", "ui-sans-serif", "system-ui", "sans-serif"],
+            },
+        },
     },
     plugins: [],
 };
