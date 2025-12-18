18f392a2f1bc2adef3c2efb0f4f275fd02509947
Aaron <aaronye1001@gmail.com>
2025-12-18 17:50:24 -0500

    Add nav links

---
+++ src/components/NavLinks.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    export let currentPath: string = "/";
+</script>
+
+<a class="font-medium hover:text-ctp-mauve-100 duration-200" class:text-ctp-mauve={currentPath === '/'}
+   href="/">About</a>
+<a class="font-medium hover:text-ctp-mauve-100 duration-200" class:text-ctp-mauve={currentPath === '/projects'}
+   href="/projects">Projects</a>
+<a class="font-medium hover:text-ctp-mauve-100 duration-200" class:text-ctp-mauve={currentPath === '/blog'}
+   href="/blog">Blog</a>
+<a class="font-medium hover:text-ctp-mauve-100 duration-200" class:text-ctp-mauve={currentPath === '/guestbook'}
+   href="/guestbook">Guestbook</a>
