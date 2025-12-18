f3fd8f5167fc871dda0b640ce5a4eb48a04ddab4
Aaron <aaronye1001@gmail.com>
2025-12-18 18:18:08 -0500

    Add contacts

--- src/api/increment-visits-count.ts
+++ src/api/increment-visits-count.ts
@@ -5,6 +5,14 @@ import {db} from "@/lib/database.ts";
 
 export async function POST() {
     const result = await incrementVisitsCount(db);
+
+    if (!result) {
+        return new Response(JSON.stringify({error: "Failed to increment visits count"}), {
+            headers: {"Content-Type": "application/json"},
+            status: 500
+        });
+    }
+
     return new Response(JSON.stringify(result), {
         headers: {"Content-Type": "application/json"}
     });
--- src/components/NavLinks.svelte
+++ src/components/NavLinks.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
     export let currentPath: string = "/";
 </script>
-
-<a class="font-medium hover:text-ctp-mauve-100 duration-200" class:text-ctp-mauve={currentPath === '/'}
-   href="/">About</a>
-<a class="font-medium hover:text-ctp-mauve-100 duration-200" class:text-ctp-mauve={currentPath === '/projects'}
-   href="/projects">Projects</a>
-<a class="font-medium hover:text-ctp-mauve-100 duration-200" class:text-ctp-mauve={currentPath === '/blog'}
-   href="/blog">Blog</a>
-<a class="font-medium hover:text-ctp-mauve-100 duration-200" class:text-ctp-mauve={currentPath === '/guestbook'}
-   href="/guestbook">Guestbook</a>
+<div class="flex gap-6">
+    <a class="font-medium hover:text-ctp-mauve-100 duration-200" class:text-ctp-mauve={currentPath === '/'}
+       href="/">About</a>
+    <a class="font-medium hover:text-ctp-mauve-100 duration-200" class:text-ctp-mauve={currentPath === '/projects'}
+       href="/projects">Projects</a>
+    <a class="font-medium hover:text-ctp-mauve-100 duration-200" class:text-ctp-mauve={currentPath === '/blog'}
+       href="/blog">Blog</a>
+    <a class="font-medium hover:text-ctp-mauve-100 duration-200" class:text-ctp-mauve={currentPath === '/guestbook'}
+       href="/guestbook">Guestbook</a>
+</div>
